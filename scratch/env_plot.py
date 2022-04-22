5c6c025e38978f614f027f7038f5fa231d1bf15b
schimpfen <nathanschimpf@gmail.com>
2022-04-21 21:22:26 -0400

    Cleanup for Presentation

--- scratch/env_plot.py
+++ scratch/env_plot.py
@@ -21,17 +21,21 @@ def main():
     pc = PatchCollection(rects, alpha=.5)
     ax.add_collection(pc)
 
+    # Read and plot UE and EnB locations
     df_ue = pd.read_csv("ues.txt")
     ax.scatter(df_ue['X'], df_ue['Y'], label="UEs")
     
     df_enb = pd.read_csv("enbs.txt")
     ax.scatter(df_enb['X'], df_enb['Y'], label="ENBs", alpha=.25)
-    
-    
+
+
+     
+    # Track X-Y Coordinate System relative to predefined building and axis measures
     buildingSizeX, streetWidth, buildingSizeY, buildingHeight = 250-3.5 * 2 - 3, 20, 433 - 3.5 * 2 - 3, 10
-    maxAxisX, maxAxisY = 4000, 3000
+    maxAxisX, maxAxisY = 4000, 3100
     numBuildingsX, numBuildingsY = maxAxisX / (buildingSizeX + streetWidth), maxAxisY / (buildingSizeX + streetWidth)
     
+    # Predefined Scenario Routes from mobility update (branch: nora)
     routes = [[
                 (0 + 1*buildingSizeX + 0*streetWidth,0 + 1*buildingSizeX + 0*streetWidth,  maxAxisX - 1*buildingSizeX - 0*streetWidth, 0 + 1*buildingSizeX + 0*streetWidth), 
                 (0 + 1*buildingSizeY + 0*streetWidth, 0 + 1*buildingSizeY + 0*streetWidth, 0 + 3*buildingSizeY + 2*streetWidth, 0 + 1*buildingSizeY + 0*streetWidth)
@@ -55,8 +59,9 @@ def main():
                 ]
     polys = []
 
+    # Plot each UAS Route
     for idx, route in enumerate(routes):
-        plt.plot(route[0], route[1], label="Route {}".format(idx), marker="o", linestyle='-', linewidth=3)
+        plt.plot(route[0], route[1], label="Route {}".format(idx), marker="o", linestyle='-', linewidth=3, alpha=.5)
             # for i in range(len(route)-1):
                 # plt.plot(route[0][i], route[1][i], route[0][i+1], route[1][i+1], label="Route {}".format(idx), marker="o", linestyle='-', linewidth=3)
             # a = np.stack((route[0], route[1])).T
@@ -66,11 +71,12 @@ def main():
     # pcPoly = PatchCollection(polys)
     # ax.add_collection(pcPoly)
     
-    margin=20
+    margin=100; legendPad = 800
     # ax.set_xlim(df_bd['rectFromX'].min()-margin, df_bd['rectToX'].max()+margin)
     # ax.set_ylim(df_bd['rectFromY'].min()-margin, df_bd['rectToY'].max()+margin))
-    ax.set_xlim(0-margin, maxAxisX+margin); ax.set_ylim(0-margin, maxAxisY+margin)
+    ax.set_xlim(0-margin, maxAxisX+margin + legendPad); ax.set_ylim(0-margin, maxAxisY+margin)
     ax.set_title("Physical Environment Layout (t=0)", alpha=.5)
+    fig.tight_layout()
 
     ax.legend()
     fig.savefig("env.png")
